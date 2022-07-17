86a4692ee2f201da4a0be0b5f2f94671968807f5
vg15 <velya15@yandex.ru>
2022-07-17 14:25:51 +1000

    Урок 1. Применение паттернов Observer, Singleton и Delegate
    1. Добавить в проект синглтон для хранения данных о текущей сессии – Session.
    2. Добавить в него свойства:
    token: String – для хранения токена в VK,
    userId: Int – для хранения идентификатора пользователя ВК.

---
+++ VK_VitaliyGetman/Model/Session.swift
@@ -0,0 +1,16 @@
+//
+//  Session.swift
+//  VK_VitaliyGetman
+//
+//  Created by Гетьман Виталий on 17.07.2022.
+//
+
+import Foundation
+
+class Session {
+    private init() {}
+    static let instance = Session()
+    
+    var token = ""
+    var userID = 0
+}
